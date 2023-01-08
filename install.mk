d9ae2ae0d587abf3e9444d240f91e63b616298ed
jhertz <jh.furtado@campus.fct.unl.pt>
2023-01-08 23:25:51 +0000

    changes to install.mk

--- install.mk
+++ install.mk
@@ -3,11 +3,11 @@
 #####################################################
 
 # TODO: change the two variables below for your situation
-FS_BASE_DIR :=
-RC_FILE     :=
+FS_BASE_DIR := target
+RC_FILE     := jh_profile
 
 # to upload my local variables, don't worry about it :)
--include .myvars.mk
+#-include .myvars.mk
 include makefile
 
 FS_EXE_DIR  := $(FS_BASE_DIR)/bin
@@ -21,7 +21,8 @@ all: install
 
 install: deploy
 ifdef RC_FILE
-	echo "\nexport FS_EXE=$(FS_EXE_FILE) " >> $(RC_FILE)
+	echo "\n# added by FS install script" >> $(RC_FILE)
+	echo "export FS_EXE=$(FS_EXE_FILE) " >> $(RC_FILE)
 	echo "source $(FS_SCRIPT_FILE) " >> $(RC_FILE)
 else
 $(error RC_FILE is not define. Please read the TODOS.)
