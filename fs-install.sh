52b9b5d3df8181ada64468cf9e085d7a1f3d48d2
jhertz <jh.furtado@campus.fct.unl.pt>
2023-01-14 00:04:22 +0000

    last changes to fs-install.sh. Now we need to change the REAME.md file

--- fs-install.sh
+++ fs-install.sh
@@ -9,7 +9,7 @@ magenta='\033[0;35m'
 cyan='\033[0;36m'
 clear='\033[0m'
 
-# background colors
+# background colors also taken from the net
 bg_red='\033[0;41m'
 bg_green='\033[0;42m'
 bg_yellow='\033[0;43m'
@@ -17,14 +17,16 @@ bg_blue='\033[0;44m'
 bg_magenta='\033[0;45m'
 bg_cyan='\033[0;46m'
 
-# important variables
+# important variables:
+FS_DIR_NAME=fs-nav # name of the fs-navegation folder in the FS-BASE-DIR folder
+# folder where fs-navegation will be instaled
 FS_BASE_DIR=
-FS_DIR_NAME=fs-nav
+# name of the terminal rc-file (run command file)
 RC_FILE=
-# var used to save error ouput
+# var used to save error ouput, just like errno ;)
 err=
 
-
+# prints an error message and exits the program
 function error(){
 	echo -e "${red}ERROR:"
 
@@ -36,7 +38,6 @@ function error(){
 		echo -ne $blue
 		echo "COMANDLINE-ERROR: $err"
 	fi
-
 
 	exit 1
 }
@@ -50,7 +51,8 @@ function get_full_name(){
     cd $old
 }
 
-function add_dir(){
+# concatenates the name of a subdir to another dir
+function concat_dir(){
     local dir1=$1 
     local dir2=$2
 
@@ -61,6 +63,8 @@ function add_dir(){
     fi
 }
 
+# creates dirs passed as arguments if they don't exits
+# returns: 1 if couldn't create one of the dirs or 0 if everying went well
 function create_dirs(){
 	for dir in "$@"; do
 		if ! [ -d $dir ]; then
@@ -71,29 +75,49 @@ function create_dirs(){
 	return 0
 }
 
+# prints information about the base-dir and rc-file
 function print_init_info(){
 	echo "------------------------"
 	echo -e "${cyan}** install info **${clear}"
 	echo -e "${blue}base-dir:${clear} ${green}$FS_BASE_DIR${clear}"
 	echo -ne "${blue}rc-file :${clear} "
 
-	# TODO: look what if a RC_FILE is provided
-	if [ -z $FS_EXE ] ; then
-	   [  -n "$RC_FILE" ] && echo -e "${green}$RC_FILE$" || echo -e "${red}<none>"   
-	   echo -e ${clear}
+	if [ -z $RC_FILE ]; then
+	   [ -n $FS_EXE ] && echo "---" || echo -e "${red}<none>" 
 	else
-		echo "---"
+		echo -e "${green}$RC_FILE"
 	fi
-	echo -e "------------------------\n"
+
+	echo -e "${clear}------------------------\n"
 }
 
+# checks the script has wirte access in the diretory/file
+# passed as argument
+function check_write_acess(){
+	local dir=$1	
+
+	if ! [ -w "$dir" ]; then  
+		error "Doesn't have write access to: $dir" \
+              "You need to use ${blue}sudo${red} before the command."
+	fi
+}
+
+
+# copies files to their respective place
+# and reports an error whenever such happens
 function install_fs(){
+
+
+	# checks for write access to FS_BASE_DIR and RC_FILE
+	check_write_acess "$FS_BASE_DIR"
+	if [ -n "$RC_FILE" ] ; then
+	   [ -d "$RC_FILE" ] && error "The rc-file should be a file not a directory"
+		check_write_acess "$RC_FILE"
+	fi
 
 	# prints info about the base-dir and the rc-file
 	print_init_info
-
-	# TODO: check if the RC_FILE is valid and if we can write on it
-
+	
     if ! err=$(make install 2>&1) ; then
         error "Unable to buildig the project. Be sure you are in the right directory" 
     fi
@@ -104,7 +128,7 @@ function install_fs(){
     local src_exe=${_values[1]}
 
 	# get the target dirs names
-    local target_base_dir=$(add_dir $FS_BASE_DIR $FS_DIR_NAME )
+    local target_base_dir=$(concat_dir $FS_BASE_DIR $FS_DIR_NAME )
     local target_bin_dir=$target_base_dir/bin
 
 	# the target name files
@@ -116,7 +140,7 @@ function install_fs(){
 		error  "Unable to create one or both of the fs-navegation diretories:" \
 			   "   -> $target_base_dir" \
 			   "   -> $target_bin_dir" \
-			   "you may need to use sudo or there's a file with the same name" 
+			   "There might be some files with the same name as these dirs." 
 			   
 	fi
 
@@ -130,25 +154,24 @@ function install_fs(){
     cp $src_script $fs_script
 
 	echo -e "${green}*** files copied!! ***${clear}\n"
-
-
-	if [[ -z $FS_EXE ]]; then
+	
+	if [[ -z "$FS_EXE" ]] || [[ -n "$RC_FILE" ]]; then
 
 		# get the full dir name
 		target_base_dir=$(get_full_name $target_base_dir)
 
-    	local commands=$( \
+		local commands=$( \
 			echo -n "BASE_DIR=$( get_full_name $FS_BASE_DIR)\n";
 			echo -n "export FS_EXE=\$BASE_DIR/$fs_exe\n";
 			echo -n "source \$BASE_DIR/$fs_script\n"
-		)
+		)	
 
 		echo -e "${cyan}** commands for your rc-file **${clear}"
 
-		if [[ -n $RC_FILE ]] ; then
-			commands="\n\n#Added by fs-install\n$commands"
-			echo -e "appending: ${bg_blue}$commands${clear}\n\nto your ${blue}rc-file${clear}"
-			echo -e $commands >> $RC_FILE
+		if [[ -n "$RC_FILE" ]] ; then
+			commands="\n#Added by fs-install\n$commands"
+			echo -e "appending: \n${bg_blue}$commands${clear}\nto your ${blue}rc-file${clear}"
+			echo -e "\n$commands" >> $RC_FILE
 			echo "Restart your terminal and you can run the program."
 		else
 			echo "You need to copy the following to your rc-file:"
@@ -163,6 +186,10 @@ function install_fs(){
 
 }
 
+# TODO: change to we only pop up the RC-thing if:
+# the folder where
+# the fs-exe was not set
+# the RC_FILE was specified
 
 function main(){
 	echo -e "${blue}--- fs-install running ---${clear}"
@@ -177,16 +204,15 @@ function main(){
 
 	if [ $# -gt 0 ] ; then
 		FS_BASE_DIR=$1
-		[ $# -gt 1 ] && RC_FILE=$2 # TODO: check if we can write in the rc_file
+		[ $# -gt 1 ] && RC_FILE=$2
 	fi
 
 	if [ -z "$FS_BASE_DIR" ]; then
-	# TODO: if FS_EXE is defined get the dir from there
 	# TODO: look at the message below
 	# change error message and change the plane where you will use the
 		error \
-		"You have to specify at least the base-dir (directory where to install the program)" \
-		"Call this way: [ fs-install.sh <base-dir> <rc-file (optional)> ]"
+		"You have to specify at least the base-dir (directory to install the program)" \
+		"usage: [ fs-install.sh <base-dir> <rc-file (optional)> ]"
 	elif ! [ -d "$FS_BASE_DIR" ] ; then
 		error "no such directory: $FS_BASE_DIR"
 	fi
